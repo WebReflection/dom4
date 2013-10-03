0b83b31c73354deb2964c36d9d6ee64116f8ddfb
Andrea Giammarchi <agiammarchi@twitter.com>
2013-10-03 16:03:28 -0700

    trying to fix asha

--- test/src/dom4.max.js
+++ test/src/dom4.max.js
@@ -194,7 +194,7 @@ THE SOFTWARE.
       // no other way to reach original methods in iOS 5.1
       ElementPrototype = DOMTokenList.constructor.prototype;
       if (!('add' in ElementPrototype)) {
-        alert(window.DOMTokenList);
+        alert(window.DOMTokenList.prototype.add);
       }
       verifyToken = function (original) {
         return function () {
